9df510b616b99cdddb28e1b3588d5e32cae35640
gabrielpenajo <gabrielpenajo13@gmail.com>
2023-03-08 20:46:26 -0300

    configure project structure

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            Pagina inicial
+        </div>
+    );
+}
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Unbounded', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
