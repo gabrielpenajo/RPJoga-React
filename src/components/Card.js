a5b60fa5357d18d1f602b4c131080345247457a2
gabrielpenajo <gabrielpenajo13@gmail.com>
2023-03-11 17:34:21 -0300

    move modal change events to a function

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,13 @@ import { useState } from "react";
 export function Card({ title, image, imageAlt, content, details }) {
     const [isOpen, setOpen] = useState(false);
 
+    const handleModalChange = (event) => {
+        setOpen(!isOpen)
+        event.stopPropagation()
+    }
+
     const cardRef = (
-        <div id="card" onClick={() => setOpen(true)}
+        <div id="card" onClick={handleModalChange}
             class="w-full overflow-y-scroll sm:overflow-y-hidden
                 h-32 border border-gray-300 hover:shadow-lg transition-shadow
                 flex flex-row gap-2 rounded-lg cursor-pointer p-2">
@@ -22,13 +27,14 @@ export function Card({ title, image, imageAlt, content, details }) {
         </div>
     )
     const modalRef = (
-        <dialog id="card-modal" open
-            class="absolute min-h-full min-w-full drop-shadow-2xl bg-transparent
-                   flex justify-center">
+        <dialog id="card-modal"
+            class="absolute min-w-full drop-shadow-2xl
+                   flex justify-center bg-transparent"
+            onMouseLeave={handleModalChange}>
             <div class="grid w-[75%] h-fit bg-white p-3 rounded-lg">
                 <div id="header" class="flex flex-row justify-between">
                     <h3 class="font-bold text-xl">{title}</h3>
-                    <button onClick={() => setOpen(false)}>X</button>
+                    <button onClick={handleModalChange}>X</button>
                 </div>
                 <div class="flex flex-row gap-2">
                     <div class="flex flex-shrink-0">
@@ -50,14 +56,14 @@ export function Card({ title, image, imageAlt, content, details }) {
 
     if (isOpen)
         return (
-            <>
+            <div>
                 {modalRef}
                 {cardRef}
-            </> 
+            </div> 
         );
     else return (
-        <>
+        <div>
             {cardRef}
-        </>
+        </div>
     );
 }
