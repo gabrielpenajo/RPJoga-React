434891735b0661285591e6491f156b0af871c95e
gabrielpenajo <gabrielpenajo13@gmail.com>
2023-03-11 15:13:08 -0300

    create card component

---
+++ src/components/Card.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+export function Card({ title, image, imageAlt, content, details }) {
+    const [isOpen, setOpen] = useState(false);
+
+    const cardRef = (
+        <div id="card" onClick={() => setOpen(true)}
+            class="w-full overflow-y-scroll sm:overflow-y-hidden
+                h-32 border border-gray-300 hover:shadow-lg transition-shadow
+                flex flex-row gap-2 rounded-lg cursor-pointer p-2">
+            <div class="flex flex-shrink-0">
+                <img class="w-28 h-28 rounded-md"
+                    src={image}
+                    alt={imageAlt}/>
+                </div>
+                <div class="flex flex-col flex-grow gap-2">
+                <h3 class="font-bold text-xl">{title}</h3>
+                <div class="flex flex-col">
+                    {content}
+                </div>
+            </div>
+        </div>
+    )
+    const modalRef = (
+        <dialog id="card-modal" open
+            class="absolute min-h-full min-w-full top-0 drop-shadow-2xl bg-transparent
+                   flex justify-center">
+            <div class="grid w-[75%] h-fit bg-white p-3 rounded-lg">
+                <div id="header" class="flex flex-row justify-between">
+                    <h3 class="font-bold text-xl">{title}</h3>
+                    <button onClick={() => setOpen(false)}>X</button>
+                </div>
+                <div class="flex flex-row gap-2">
+                    <div class="flex flex-shrink-0">
+                        <img class="w-28 h-28 rounded-md"
+                            src={image}
+                            alt={imageAlt}/>
+                        </div>
+                        <div class="flex flex-col flex-grow gap-2">
+                        <div class="flex flex-col">
+                            {content}
+                            {details}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </dialog>
+    )
+
+
+    if (isOpen)
+        return (
+            <div class="wrapper">
+                {cardRef}
+                {modalRef}
+            </div> 
+        );
+    else return (
+        <div class="wrapper">
+            {cardRef}
+        </div>
+    );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,24 @@
+import { Card } from "../components/Card";
+
 function Home() {
     return (
         <div>
-            Pagina inicial
+            <Card
+                title={"Oi"}
+                content={<p>Olá</p>}
+                image={"https://play-lh.googleusercontent.com/8ddL1kuoNUB5vUvgDVjYY3_6HwQcrg1K2fd_R8soD-e2QYj8fT9cfhfh3G0hnSruLKec"}
+                imageAlt={"Amogus"}
+                details={
+                    <div class="flex flex-row gap-2">
+                        <label for="tags" class="font-semibold">Tags:</label>
+                        <ul>
+                            <li>Terror</li>
+                            <li>Suspense</li>
+                            <li>Fred Desimpedidos</li>
+                        </ul>
+                    </div>
+                }
+            />
         </div>
     );
 }
