9c6fd6573c65f5784f5148b80a2d911f3bd98bc1
gabrielpenajo <gabrielpenajo13@gmail.com>
2023-03-11 21:19:38 -0300

    add logo for navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export function Navbar() {
+    return (
+        <div class="bg-main">
+            <div class="mx-auto max-w-7xl px-6">
+                <div class="flex items-center justify-between py-4 md:justify-start md:space-x-10">
+                    <div class="flex justify-start lg:w-0 lg:flex-1">
+                        <a href="/rpgs.html">
+                            <span class="sr-only">RPJoga</span>
+                            <img class="h-8 w-8 sm:h-10 sm:w-10" src={`${process.env.PUBLIC_URL}/logo.png`} alt="" />
+                        </a>
+                    </div>
+                    <label for="menuToggle" class="-my-2 -mr-2 md:hidden bg-main hover:bg-secondary rounded-md">
+                        <button type="button"
+                            class="inline-flex items-center justify-center p-2 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                            aria-expanded="false">
+                            <span class="sr-only">Abrir menu</span>
+                            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg>
+                        </button>
+                    </label>
+                    <nav class="hidden space-x-10 md:flex">
+                        <a href="/rpgs.html" class="text-base font-medium text-gray-400 hover:text-secondary">RPGs</a>
+                        <a href="/profile.html" class="text-base font-medium text-white hover:text-secondary">Meu perfil</a>
+                    </nav>
+                    <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+                        <a href="/login.html" class="whitespace-nowrap text-base font-medium text-white hover:text-secondary">Sair</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
