bd8c6f4911d21770dc5e8608be9d81f2981761c0
gabrielpenajo <gabrielpenajo13@gmail.com>
2023-03-11 21:19:54 -0300

    add color variables for tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,18 @@ module.exports = {
     extend: {
       fontFamily: {
         'sans': ['Inter', ...defaultTheme.fontFamily.sans],
-      }
+      },
+      colors: {
+        'main': '#682d63',
+        'secondary': '#7576bd',
+        'success': '#5fb49c',
+        'warning': '#ffff70',
+        'error': '#e6614c',
+        'neutral': '#2c2c2c',
+        'white': '#fff',
+        'black': '#000',
+        'grey': '#d9d9d9',
+      },
     },
   },
   plugins: [],
