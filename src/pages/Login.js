b5ea0e54b25f18e7e07697eadd7ba267e194ee9a
isarbela <panzeref@gmail.com>
2023-03-13 10:04:28 -0300

    Added fields and buttons login and signup pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,51 @@
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+
+const ColoredLine = ({ color }) => (
+    <hr size="5" width="20%" color={color} />
+);
+
 function Login() {
     return (
-        <div>
-            Logar
+        <div className="bg-main min-h-screen overflow-auto">
+            <div className="flex flex-col p-4">
+                <div className="h-12 w-full flex flex-row gap-2">
+                    <a href="/"
+                        className="outline outline-2 bg-transparent ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-secondary">Voltar</a>
+                </div>
+            </div>
+            <div className="flex flex-row justify-center min-h-1/2">
+                <div className="basis-1/2">
+                    <div className="flex min-h-full items-center justify-center">
+                        <div className="w-full max-w-md space-y-8">
+                            <div className="flex flex-row justify-center m-2 p-2">
+                                <span className="sr-only">RPJoga</span>
+                                <h2 className="font-semibold text-white text-6xl">RPJoga</h2>
+                            </div>
+                            <form className="mt-8 space-y-6" action="/rpgs">
+                                <div className="space-y-px rounded-md shadow-sm">
+                                    <div className="mb-6">
+                                        <Input id="email" value="" label="Email" type="email" size="full" onChange={e => { }} />
+                                    </div>
+                                    <div className="mb-4">
+                                        <Input id="senha" value="" label="Senha" type="password" size="full" onChange={e => { }}/>
+                                    </div>
+                                </div>
+                                <div>      
+                                    <Button label={"Fazer login"} color="success" action="none" type={"submit"} /> 
+                                </div>
+                            </form>
+                            <div className="flex flex-col items-center justify-center">
+                                <ColoredLine color="white" />
+                                <span className="text-white m-3">Ainda não tem uma conta?</span>
+                                <form className="w-full" action="/signUp">
+                                    <Button label={"Cadastre-se"} color="secondary" action="none" />
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,49 @@
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+
 function SignUp() {
     return (
-        <div>
-            Cadastro
+        <div className="bg-main min-h-screen overflow-auto">
+             <div className="flex flex-col p-4">
+                <div className="h-12 w-full flex flex-row">
+                    <a href="/"
+                        className="outline outline-2 bg-transparent ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-secondary">Voltar</a>
+                </div>
+            </div>
+            <div className="flex flex-col items-center mb-6 mt-6">
+                <h2 className="font-semibold text-white text-6xl">Cadastro</h2>
+            </div>
+            <div className="w-screen flex flex-row justify-center">
+                <form className="flex flex-col items-center md:w-2/3" action="/">
+                        <div className="w-full flex flex-col justify-center items-center md:flex-row">
+                            <div className="w-full flex flex-col p-2 md:basis-1/2">
+                                <div className="mb-4">
+                                    <Input id="fullname" value="" label="Nome Completo" type="text" size="full" onChange={e => { }} />
+                                </div>
+                                <div className="mb-4">
+                                    <Input id="password" value="" label="Senha" type="password" size="full" onChange={e => { }} />
+                                </div>
+                                <div className="md:mb-4">
+                                <Input id="birthDay" value="" label="Data de Nascimento" type="date" size="full" onChange={e => { }} />
+                                </div>
+                            </div>
+                            <div className="w-full flex flex-col p-2 md:basis-1/2">
+                                <div className="mb-4">
+                                <Input id="email" value="" label="Email" type="text" size="full" onChange={e => { }} />
+                                </div>
+                                <div className="mb-4">
+                                <Input id="confirmPassword" value="" label="Confirmar senha" type="password" size="full" onChange={e => { }} />
+                                </div>
+                                <div className="mb-4">
+                                <Input id="username" value="" label="Nome de usuário" type="text" size="full" onChange={e => { }} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex flex-col w-full justify-center items-center mb-4 md:w-1/3">
+                            <Button label={"Confirmar Cadastro"} color="success" action="none" type={'submit'}/>
+                        </div>
+                </form>
+            </div>
         </div>
     );
 }
