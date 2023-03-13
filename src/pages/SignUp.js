cae56e4c3b2373f5287c4943b2a01be2a7f1a448
Gabriel Penajo <gabriel.penajo@sflabs.com.br>
2023-03-13 18:51:11 -0300

    create basic sign up page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,27 @@
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { useState } from "react";
 
 function SignUp() {
+    const [birthdate, setBirthdate] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [fullname, setFullname] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+
+    const handlePasswordConfirm = (e) => {
+        const passwordValue = e.target.value
+
+        setPasswordConfirm(passwordValue)
+        
+        if (passwordValue != password) {
+            console.log('error')
+        } 
+    }
+
     return (
+    <>
         <div className="bg-main min-h-screen overflow-auto">
              <div className="flex flex-col p-4">
                 <div className="h-12 w-full flex flex-row">
@@ -10,6 +29,11 @@ function SignUp() {
                         className="outline outline-2 bg-transparent ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-secondary">Voltar</a>
                 </div>
             </div>
+            <img
+                src={`${process.env.PUBLIC_URL}/assets/images/Castle-cuate.svg`}
+                className="absolute bg-no-repeat h-full opacity-10 bottom-0 right-0 pointer-events-none -z-1"
+                alt="Imagem de fundo"
+            />
             <div className="flex flex-col items-center mb-6 mt-6">
                 <h2 className="font-semibold text-white text-6xl">Cadastro</h2>
             </div>
@@ -18,24 +42,24 @@ function SignUp() {
                         <div className="w-full flex flex-col justify-center items-center md:flex-row">
                             <div className="w-full flex flex-col p-2 md:basis-1/2">
                                 <div className="mb-4">
-                                    <Input id="fullname" value="" label="Nome Completo" type="text" size="full" onChange={e => { }} />
+                                    <Input id="fullname" value={fullname} label="Nome Completo" type="text" size="full" onChange={e => setFullname(e.target.value)} />
                                 </div>
                                 <div className="mb-4">
-                                    <Input id="password" value="" label="Senha" type="password" size="full" onChange={e => { }} />
+                                    <Input id="password" value={password} label="Senha" type="password" size="full" onChange={e => setPassword(e.target.value)} />
                                 </div>
                                 <div className="md:mb-4">
-                                <Input id="birthDay" value="" label="Data de Nascimento" type="date" size="full" onChange={e => { }} />
+                                <Input id="birthDay" value={birthdate} label="Data de Nascimento" type="date" size="full" onChange={e => setBirthdate(e.target.value)} />
                                 </div>
                             </div>
                             <div className="w-full flex flex-col p-2 md:basis-1/2">
                                 <div className="mb-4">
-                                <Input id="email" value="" label="Email" type="text" size="full" onChange={e => { }} />
+                                <Input id="email" value={email} label="Email" type="text" size="full" onChange={e => setEmail(e.target.value)} />
                                 </div>
                                 <div className="mb-4">
-                                <Input id="confirmPassword" value="" label="Confirmar senha" type="password" size="full" onChange={e => { }} />
+                                <Input id="confirmPassword" value={passwordConfirm} label="Confirmar senha" type="password" size="full" onChange={handlePasswordConfirm} />
                                 </div>
                                 <div className="mb-4">
-                                <Input id="username" value="" label="Nome de usuário" type="text" size="full" onChange={e => { }} />
+                                <Input id="username" value={username} label="Nome de usuário" type="text" size="full" onChange={e => setUsername(e.target.value)} />
                                 </div>
                             </div>
                         </div>
@@ -45,6 +69,7 @@ function SignUp() {
                 </form>
             </div>
         </div>
+    </>
     );
 }
 
