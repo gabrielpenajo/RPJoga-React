99db15812ed79f4b5c1e2604a6141b4281acedb3
Gabriel Penajo <gabriel.penajo@sflabs.com.br>
2023-03-12 21:00:53 -0300

    card: refactor close icon

--- src/components/Card.js
+++ src/components/Card.js
@@ -34,7 +34,16 @@ export function Card({ title, image, imageAlt, content, details }) {
             <div class="grid w-[75%] h-fit bg-white p-3 rounded-lg">
                 <div id="header" class="flex flex-row justify-between">
                     <h3 class="font-bold text-xl">{title}</h3>
-                    <button onClick={handleModalChange}>X</button>
+                    <button
+                        type="button" class="inline-flex hover:text-gray-500 text-gray-400 items-center justify-center"
+                        onClick={handleModalChange}
+                    >
+                        <span class="sr-only">Fechar menu</span>
+                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
                 </div>
                 <div class="flex flex-row gap-2">
                     <div class="flex flex-shrink-0">
