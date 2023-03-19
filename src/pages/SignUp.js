3332759ce463827427715cc82f4992cd4e5dc01b
Gabriel Penajo <gabriel.penajo@sflabs.com.br>
2023-03-18 22:14:00 -0300

    implement sign up validation

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,8 @@
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { registerUser } from "../services/UserService";
+import { Navigate } from "react-router-dom";
 
 function SignUp() {
     const [birthdate, setBirthdate] = useState('')
@@ -19,6 +21,9 @@ function SignUp() {
     const [passwordConfirm, setPasswordConfirm] = useState('')
     const [isPasswordValid, setIsPasswordValid] = useState(false)
     
+    const [isEmailInUse, setIsEmailInUse] = useState(false)
+    const [success, setSuccess] = useState(false)
+
     const isValid = () => {
         return (isPasswordValid && isFullnameValid && isEmailValid && isUsernameValid && isBirthdateValid)
     }
@@ -47,8 +52,9 @@ function SignUp() {
     }
 
     const handleEmail = (e) => {
-        setEmail(e.target.value)
-        setIsEmailValid(e.target.value.length > 0)
+        const emailInput = e.target.value
+        setEmail(emailInput)
+        setIsEmailValid(emailInput.length > 0 && emailInput.search(/@/g) !== -1)
     }
 
     const handleUsername = (e) => {
@@ -56,8 +62,28 @@ function SignUp() {
         setIsUsernameValid(e.target.value.length > 0)
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const user = {
+            fullname: fullname,
+            email: email,
+            username: username,
+            password: password,
+            birth_date: birthdate
+        }
+        const response = await registerUser(user)
+        if (response.status === 500) {
+            setIsEmailInUse(true)
+        } else if (response.status === 200) {
+            setSuccess(true)
+        }
+    }
+
     return (
     <>
+        {success && (
+            <Navigate to="/login" replace={true} />
+        )}
         <div className="bg-main min-h-screen overflow-auto">
              <div className="flex flex-col p-4">
                 <div className="h-12 w-full flex flex-row">
@@ -74,34 +100,37 @@ function SignUp() {
                 <h2 className="font-semibold text-white text-6xl">Cadastro</h2>
             </div>
             <div className="w-screen flex flex-row justify-center">
-                <form className="flex flex-col items-center md:w-2/3" action="/">
-                        <div className="w-full flex flex-col justify-center items-center md:flex-row">
-                            <div className="w-full flex flex-col p-2 md:basis-1/2">
-                                <div className="mb-4">
-                                    <Input id="fullname" value={fullname} label="Nome Completo" type="text" size="full" onChange={handleFullname} />
-                                </div>
-                                <div className="mb-4">
-                                    <Input id="password" value={password} label="Senha" type="password" size="full" onChange={handlePassword} />
-                                </div>
-                                <div className="md:mb-4">
-                                <Input id="birthDay" value={birthdate} label="Data de Nascimento" type="date" size="full" onChange={handleBirthdate} />
-                                </div>
+                <form className="flex flex-col items-center md:w-2/3" onSubmit={handleSubmit}>
+                    { isEmailInUse &&
+                        <p className="w-full justify-center bg-red-700 text-white rounded-lg h-8 items-center flex mb-8">E-mail já cadastrado</p>
+                    }
+                    <div className="w-full flex flex-col justify-center items-center md:flex-row">
+                        <div className="w-full flex flex-col p-2 md:basis-1/2">
+                            <div className="mb-4">
+                                <Input id="fullname" value={fullname} label="Nome Completo" type="text" size="full" onChange={handleFullname} />
                             </div>
-                            <div className="w-full flex flex-col p-2 md:basis-1/2">
-                                <div className="mb-4">
-                                <Input id="email" value={email} label="Email" type="text" size="full" onChange={handleEmail} />
-                                </div>
-                                <div className="mb-4">
-                                <Input id="confirmPassword" value={passwordConfirm} label="Confirmar senha" type="password" size="full" onChange={handlePasswordConfirm} />
-                                </div>
-                                <div className="mb-4">
-                                <Input id="username" value={username} label="Nome de usuário" type="text" size="full" onChange={handleUsername} />
-                                </div>
+                            <div className="mb-4">
+                                <Input id="password" value={password} label="Senha" type="password" size="full" onChange={handlePassword} />
+                            </div>
+                            <div className="md:mb-4">
+                            <Input id="birthDay" value={birthdate} label="Data de Nascimento" type="date" size="full" onChange={handleBirthdate} />
                             </div>
                         </div>
-                        <div className="flex flex-col w-full justify-center items-center mb-4 md:w-1/3">
-                            <Button label={"Confirmar Cadastro"} color="success" action="none" type={'submit'} isValid={isValid()}/>
+                        <div className="w-full flex flex-col p-2 md:basis-1/2">
+                            <div className="mb-4">
+                            <Input id="email" value={email} label="Email" type="text" size="full" onChange={handleEmail} />
+                            </div>
+                            <div className="mb-4">
+                            <Input id="confirmPassword" value={passwordConfirm} label="Confirmar senha" type="password" size="full" onChange={handlePasswordConfirm} />
+                            </div>
+                            <div className="mb-4">
+                            <Input id="username" value={username} label="Nome de usuário" type="text" size="full" onChange={handleUsername} />
+                            </div>
                         </div>
+                    </div>
+                    <div className="flex flex-col w-full justify-center items-center mb-4 md:w-1/3">
+                        <Button label={"Confirmar Cadastro"} color="success" action="none" type={'submit'} isValid={isValid()}/>
+                    </div>
                 </form>
             </div>
         </div>
