7e1498f04856e6a1afe39f046e86e43eb4cdf9ff
Gabriel Penajo <gabriel.penajo@sflabs.com.br>
2023-03-18 21:42:38 -0300

    replace class -> className

--- src/pages/Rpgs.js
+++ src/pages/Rpgs.js
@@ -30,9 +30,9 @@ function Rpgs() {
     return (
         <>
             <Navbar focusedLink={"rpg"}></Navbar>
-            <div class="flex flex-col w-full items-center">
-                <div class="flex flex-col md:w-2/3 max-w-4xl">
-                    <div class="flex flex-col items-center gap-8 p-4 w-full">
+            <div className="flex flex-col w-full items-center">
+                <div className="flex flex-col md:w-2/3 max-w-4xl">
+                    <div className="flex flex-col items-center gap-8 p-4 w-full">
                         {rpgCards}
                     </div>
                 </div>
